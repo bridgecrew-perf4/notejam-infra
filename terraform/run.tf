679d2e43799192d901986be2c9c475d4fd02b267
Lupu Pitkänen <lupu@ops.host>
2020-11-11 23:09:44 +0200

    ignore changes to image as cloud build updates it continuously

--- terraform/run.tf
+++ terraform/run.tf
@@ -58,7 +58,7 @@ resource "google_cloud_run_service" "default" {
       template[0].metadata[0].annotations["client.knative.dev/user-image"],
       template[0].metadata[0].annotations["run.googleapis.com/client-name"],
       template[0].metadata[0].annotations["run.googleapis.com/client-version"],
-      # template[0].spec[0].containers[0].image,
+      template[0].spec[0].containers[0].image,
     ]
   }
   depends_on = [google_cloudbuild_trigger.deploy]
